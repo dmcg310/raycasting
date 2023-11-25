d9921b7685a1374e42ef67fd0e62ce41e8ec25c3
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-11-25 13:14:17 +0000

    feat: player movement

--- js/raycast.js
+++ js/raycast.js
@@ -8,12 +8,13 @@ const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 class Map {
   constructor() {
     this.grid = [
+      // 15
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
       [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
       [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1], // 11
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
       [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
@@ -36,16 +37,93 @@ class Map {
   }
 }
 
+class Player {
+  constructor() {
+    this.x = WINDOW_WIDTH / 2;
+    this.y = WINDOW_HEIGHT / 2;
+    this.radius = 3;
+    this.turnDirection = 0; // Left (-1) Right (+1)
+    this.walkDirection = 0; // Back (-1) Forward (+1)
+    this.rotationAngle = Math.PI / 2; // 90 degrees
+    this.moveSpeed = 2.0; // 2 pixels per frame
+    this.rotationSpeed = 2 * (Math.PI / 180); // 2 degrees per frame
+  }
+
+  update() {
+    this.rotationAngle += this.turnDirection * this.rotationSpeed;
+
+    let moveStep = this.walkDirection * this.moveSpeed;
+    let newX = this.x + Math.cos(this.rotationAngle) * moveStep;
+    let newY = this.y + Math.sin(this.rotationAngle) * moveStep;
+
+    let gridX = Math.floor(newX / TILE_SIZE);
+    let gridY = Math.floor(newY / TILE_SIZE);
+
+    if (
+      gridX >= 0 &&
+      gridX < MAP_NUM_COLS &&
+      gridY >= 0 &&
+      gridY < MAP_NUM_ROWS
+    ) {
+      if (grid.grid[gridY][gridX] == 0) {
+        this.x = newX;
+        this.y = newY;
+      }
+    }
+  }
+
+  render() {
+    noStroke();
+    fill("red");
+    circle(this.x, this.y, this.radius);
+    stroke("red");
+    line(
+      this.x,
+      this.y,
+      this.x + Math.cos(this.rotationAngle) * 30,
+      this.y + Math.sin(this.rotationAngle) * 30,
+    );
+  }
+}
+
 let grid = new Map();
+let player = new Player();
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    player.walkDirection = +1;
+  } else if (keyCode == DOWN_ARROW) {
+    player.walkDirection = -1;
+  } else if (keyCode == RIGHT_ARROW) {
+    player.turnDirection = +1;
+  } else if (keyCode == LEFT_ARROW) {
+    player.turnDirection = -1;
+  }
+}
+
+function keyReleased() {
+  if (keyCode == UP_ARROW) {
+    player.walkDirection = 0;
+  } else if (keyCode == DOWN_ARROW) {
+    player.walkDirection = 0;
+  } else if (keyCode == RIGHT_ARROW) {
+    player.turnDirection = 0;
+  } else if (keyCode == LEFT_ARROW) {
+    player.turnDirection = 0;
+  }
+}
 
 function setup() {
   createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
-function update() {}
+function update() {
+  player.update();
+}
 
 function draw() {
   update();
 
   grid.render();
+  player.render();
 }
