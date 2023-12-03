e188b5c81741cdd39dcd8b4a2efa197d19e92d27
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-12-03 21:03:47 +0000

    feat: minimap

--- js/raycast.js
+++ js/raycast.js
@@ -6,6 +6,7 @@ const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 const FOV_ANGLE = 60 * (Math.PI / 180);
 const WALL_STRIP_WIDTH = 1;
 const NUM_RAYS = WINDOW_WIDTH / WALL_STRIP_WIDTH;
+const MINIMAP_SCALE_FACTOR = 0.2;
 
 class Map {
   constructor() {
@@ -41,7 +42,12 @@ class Map {
 
         stroke("#222");
         fill(tileColor);
-        rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+        rect(
+          MINIMAP_SCALE_FACTOR * tileX,
+          MINIMAP_SCALE_FACTOR * tileY,
+          MINIMAP_SCALE_FACTOR * TILE_SIZE,
+          MINIMAP_SCALE_FACTOR * TILE_SIZE,
+        );
       }
     }
   }
@@ -74,17 +80,19 @@ class Player {
 
   render() {
     noStroke();
-    fill("red");
-    circle(this.x, this.y, this.radius);
-    /*
-        stroke("red");
-        line(
-            this.x,
-            this.y,
-            this.x + Math.cos(this.rotationAngle) * 30,
-            this.y + Math.sin(this.rotationAngle) * 30
+    fill("blue");
+    circle(
+      MINIMAP_SCALE_FACTOR * this.x,
+      MINIMAP_SCALE_FACTOR * this.y,
+      MINIMAP_SCALE_FACTOR * this.radius,
     );
-    */
+    stroke("blue");
+    line(
+      MINIMAP_SCALE_FACTOR * this.x,
+      MINIMAP_SCALE_FACTOR * this.y,
+      MINIMAP_SCALE_FACTOR * (this.x + Math.cos(this.rotationAngle) * 30),
+      MINIMAP_SCALE_FACTOR * (this.y + Math.sin(this.rotationAngle) * 30),
+    );
   }
 }
 
@@ -222,7 +230,12 @@ class Ray {
 
   render() {
     stroke("rgba(255, 0, 0, 0.3)");
-    line(player.x, player.y, this.wallHitX, this.wallHitY);
+    line(
+      MINIMAP_SCALE_FACTOR * player.x,
+      MINIMAP_SCALE_FACTOR * player.y,
+      MINIMAP_SCALE_FACTOR * this.wallHitX,
+      MINIMAP_SCALE_FACTOR * this.wallHitY,
+    );
   }
 }
 
