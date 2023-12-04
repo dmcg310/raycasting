59930e06eaeb588624618f31b76e1c528e11e594
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-12-04 18:19:24 +0000

    feat: SDL window

--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -1,7 +1,55 @@
+#include "constants.h"
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int isGameRunning = FALSE;
+
+int initialiseWindow() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initialising SDL.\n");
+    return FALSE;
+  }
+
+  window = SDL_CreateWindow("raycasting", SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT,
+                            SDL_WINDOW_BORDERLESS);
+
+  if (!window) {
+    fprintf(stderr, "Error creating SDL window.\n");
+    return FALSE;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer) {
+    fprintf(stderr, "Error creating SDL renderer.\n");
+    return FALSE;
+  }
+
+  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+  return TRUE;
+}
+
+void destroyWindow() {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
 int main(void) {
-  printf("Hello world!\n");
+  isGameRunning = initialiseWindow();
+
+  // setup();
+
+  while (isGameRunning) {
+    // processInput();
+    // update();
+    // render();
+  }
+
+  destroyWindow();
 
   return 0;
 }
