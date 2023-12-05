28b83c8f0f8f2ebabcb69cdffe7b520f414925d9
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-12-05 19:39:21 +0000

    feat: drawing the map

--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -1,9 +1,35 @@
 #include "constants.h"
 
+const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+struct Player {
+  float x;
+  float y;
+  float width;
+  float height;
+  int turnDirection; // -1 left +1 right
+  int walkDirection; // -1 back +1 forward
+  float rotationAngle;
+  float walkSpeed;
+  float turnSpeed;
+} player;
+
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 int isGameRunning = FALSE;
-int playerX, playerY;
 int ticksLastFrame = 0;
 
 int initialiseWindow() {
@@ -38,8 +64,36 @@ void destroyWindow() {
   SDL_Quit();
 }
 
-void setup() { playerX = 0, playerY = 0; }
+void setup() {
+  player.x = WINDOW_WIDTH / 2.0f;
+  player.y = WINDOW_HEIGHT / 2.0f;
+  player.width = 5;
+  player.height = 5;
+  player.turnDirection = 0;
+  player.walkDirection = 0;
+  player.rotationAngle = PI / 2;
+  player.walkSpeed = 100;
+  player.turnSpeed = 45 * (PI / 180);
+}
 
+void renderMap() {
+  for (int i = 0; i < MAP_NUM_ROWS; i++) {
+    for (int j = 0; j < MAP_NUM_COLS; j++) {
+      int tileX = j * TILE_SIZE;
+      int tileY = i * TILE_SIZE;
+      int tileColor = map[i][j] != 0 ? 255 : 0;
+
+      SDL_SetRenderDrawColor(renderer, tileColor, tileColor, tileColor, 255);
+      SDL_Rect mapTileRect = {
+          tileX * MINIMAP_SCALE_FACTOR,
+          tileY * MINIMAP_SCALE_FACTOR,
+          TILE_SIZE * MINIMAP_SCALE_FACTOR,
+          TILE_SIZE * MINIMAP_SCALE_FACTOR,
+      };
+      SDL_RenderFillRect(renderer, &mapTileRect);
+    }
+  }
+}
 void processInput() {
   SDL_Event event;
   SDL_PollEvent(&event);
@@ -60,26 +114,20 @@ void processInput() {
 }
 
 void update() {
-  int timeToWait = FRAME_TIME_LENGTH - (SDL_GetTicks() - ticksLastFrame);
-
-  if (timeToWait > 0 && timeToWait <= FRAME_TIME_LENGTH) {
-    SDL_Delay(timeToWait);
-  }
+  while (!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TIME_LENGTH))
+    ;
 
   float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
   ticksLastFrame = SDL_GetTicks();
-
-  playerX += 50 * deltaTime;
-  playerY += 50 * deltaTime;
 }
 
 void render() {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
-  SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-  SDL_Rect rect = {playerX, playerY, 20, 20};
-  SDL_RenderFillRect(renderer, &rect);
+  renderMap();
+  // renderRays();
+  // renderPlayer();
 
   SDL_RenderPresent(renderer);
 }
